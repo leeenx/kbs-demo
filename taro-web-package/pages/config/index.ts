03ba02d79e8bb9d91101a2ecf30f77a98c9a360d
arhimli <arhimli@tencent.com>
2024-10-02 02:50:03 +0800

    feat: 更新

--- taro-web-package/pages/config/index.ts
+++ taro-web-package/pages/config/index.ts
@@ -13,8 +13,8 @@ registerToGlobleScope({
   enableCache: true, // 是否开启缓存
   cacheCount: 2, // 页面的缓存个数
   dslBase: 'http://127.0.0.1:9000',
-  defaultContainer: '/web-package/page/index',
-  headlessContainer: '/web-package/page-headless/index',
+  defaultContainer: '/taro-web-package/pages/index/index',
+  headlessContainer: '/taro-web-package/pages/headless/index',
   // loadingComponent: '加载中...', // 页面加载组件，可以组件替代
   // errorComponent: '失败', // 加载失败组件
 });
